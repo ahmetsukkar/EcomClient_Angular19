8a51debb5bbeea619fc5dffe1fa424b55721e364
ahmetsukkar <ahmetsukkar@gmail.com>
2025-05-10 20:42:37 +0300

    update environment.ts file

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: false,
-    apiUrl: 'https://localhost:44390/api/',
-    //apiUrl: 'https://ecomapp.runasp.net/api/',
+    //apiUrl: 'https://localhost:44390/api/',
+    apiUrl: 'https://ecomapp.runasp.net/api/',
   };
